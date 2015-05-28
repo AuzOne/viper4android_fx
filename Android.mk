c21fe26588508e4849a3248360d08eed8e3732ef
Readone !! ErcOne <android-armv7a-belalang-tempur@users.noreply.github.com>
2015-05-29 05:14:09 +0700

    fix

--- Android.mk
+++ Android.mk
@@ -7,16 +7,15 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v21
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-visualizer
-
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-
-LOCAL_JAVA_LIBRARIES := framework
 
 LOCAL_OVERRIDES_PACKAGES := DSPManager
 
 LOCAL_PACKAGE_NAME := viper4android_v2
 
-LOCAL_PRIVILEGED_MODULE := true
-
 include $(BUILD_PACKAGE)
+
+# Use the folloing include to make our test apk.
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
